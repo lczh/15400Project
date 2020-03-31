7e5fe4da7ec4566d3df74251a66489203cc196d8
lczh <lichenz@andrew.cmu.edu>
2020-03-31 00:47:21 -0400

    random_walkV1.1

--- random_walk.py
+++ random_walk.py
@@ -1,19 +1,60 @@
 import networkx as nx
+import math
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
-from treeCrossLine import gen_graph, gen_matrix
+from tree_cross_line import gen_graph, gen_matrix, gen_diag
 import sys
 
 def take_step(transition_matrix, prob):
-    return transition_matrix @ prob
+    return transition_matrix.dot(prob)
 
-if __name__ == "__main__":
-    tree = int(sys.argv[1])
-    path = int(sys.argv[2])
+# Perform random walk for iters steps, find lowest conductance sweepcut
+def find_cut(tree, path, iters, start):
     G = gen_graph(tree, path)
-    transition = gen_matrix(G)
-    p = np.zeros(tree * path, dtype = int)
-    p[0] = 1
-    for i in range(100):
-        p = take_step(transition, p)
-    print(p)
+    trans = gen_matrix(G)
+    p = np.zeros(tree * path, dtype = float)
+    p[start] = 1
+    for i in range(iters):
+        p = take_step(trans, p)
+    d = gen_diag(G)
+    normalized = list(np.divide(p, d))
+    normalized = list(enumerate(normalized))
+    normalized = sorted(normalized, key = lambda x : x[1], reverse = True)
+    B = set(list(G.nodes))
+    A = set()
+    min_cond = sys.maxsize
+    min_set = None
+    for tup in normalized:
+        vertex = tup[0]
+        A.add(vertex)
+        B.remove(vertex)
+        if len(B) == 0: break
+        cond = nx.cuts.conductance(G, A, B)
+        if cond < min_cond:
+            min_cond = cond
+            min_set = A.copy()
+    return (min_cond, min_set)
+
+def get_set(S, n, i):
+    A = []
+    base_val = i * n
+    upper = base_val + n
+    for elt in S:
+        if elt >= base_val and elt < upper:
+            A.append(elt - base_val)
+    return A
+
+def analyze(A, n):
+    '''
+    Input: a set of vertices in a tree, n indicates the max number of vertices
+    in that tree
+    '''
+    levels = int(math.log2(n)) + 1
+    output = [ [] for i in range(levels)]
+    for num in A:
+        level = int(math.log2(num + 1))
+        output[level].append(num)
+    return output
+
+
--- tree_cross_line.py
+++ tree_cross_line.py
@@ -6,16 +6,18 @@ import sys
 def gen_graph(tree_size, path_len):
     T = nx.full_rary_tree(2, tree_size)
     P = nx.path_graph(path_len)
-    return nx.cartesian_product(T, P)
+    return nx.convert_node_labels_to_integers(nx.cartesian_product(T, P))
 
 def gen_matrix(G):
     L = nx.laplacian_matrix(G).A
     A = nx.adjacency_matrix(G).A
+    I = np.identity(A.shape[0])
     D = L + A
-    return A @ np.linalg.inv(D)
+    return (A @ np.linalg.inv(D) + I) / 2
 
-if __name__=="__main__":
-    tree_size = int(sys.argv[1])
-    path_len = int(sys.argv[2])
-    G = gen_graph(tree_size, path_len)
-    print(gen_matrix(G))
+def gen_diag(G):
+    L = nx.laplacian_matrix(G).A
+    A = nx.adjacency_matrix(G).A
+    D = L + A
+    return np.diag(D)
+
